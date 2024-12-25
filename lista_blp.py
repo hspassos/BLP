e03868a49dc4f3c3f38762339ffb5b64b71ddef7
hspassos <passos.487@gmail.com>
2024-12-24 21:52:49 -0300

    dados otimizados

--- lista_blp.py
+++ lista_blp.py
@@ -9,25 +9,20 @@ mat_data = scipy.io.loadmat(file_path)
 print(mat_data)
 print(mat_data.keys())
 
-prodsMarket = pd.DataFrame(mat_data['prodsMarket'])
-prodsMarket.columns = ['prodsMarket']
+prodsMarket = pd.DataFrame({'prodsMarket': mat_data['prodsMarket'].flatten()})
 
-share = pd.DataFrame(mat_data['share'])
-share.columns = ['share']
+data = pd.DataFrame({
+    'share': mat_data['share'].flatten(),
+    'firm': mat_data['f'].flatten(),
+    'price': mat_data['pr'].flatten(),
+    'char1': mat_data['ch'][:, 0],
+    'char2': mat_data['ch'][:, 1],
+    'char3': mat_data['ch'][:, 2],
+    'char4': mat_data['ch'][:, 3],
+    'costsh1': mat_data['costShifters'][:, 0],
+    'costsh2': mat_data['costShifters'][:, 1],
+})
 
-f = pd.DataFrame(mat_data['f'])
-f.columns = ['firm']
-
-ch = pd.DataFrame(mat_data['ch'])
-ch.columns = ['char1', 'char2', 'char3', 'char4']
-
-pr = pd.DataFrame(mat_data['pr'])
-pr.columns = ['price']
-
-costShifters = pd.DataFrame(mat_data['costShifters'])
-costShifters.columns = ['costsh1', 'costsh2']
-
-data = share.join([f, ch, pr, costShifters])
 
 display(data)
 display(prodsMarket)
